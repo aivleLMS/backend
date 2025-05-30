8b53bb2c035435f8110e8402e4fadb6be381840b
ddanglehee <shee9812@naver.com>
2025-05-30 16:01:25 +0900

    chore/separate service packages

---
+++ src/main/java/com/aivle/booksystem/service/book/BookService.java
@@ -0,0 +1,21 @@
+package com.aivle.booksystem.service.book;
+
+import com.aivle.booksystem.domain.Book;
+import java.util.List;
+
+public interface BookService {
+    // 신규 도서 등록
+    Book createBook(Book book);
+
+    // 도서 목록 확인
+    List<Book> findBooks();
+
+    // 도서 상세 정보 조회
+    Book findBookById(Long id);
+
+    // 도서 수정
+    Book updateBook(Book book);
+
+    // 도서 삭제
+    void deleteBook(Long id);
+}
