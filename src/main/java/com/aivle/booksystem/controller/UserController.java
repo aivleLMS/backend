e1048e5a900081692407698032bfb63af078859d
ddanglehee <shee9812@naver.com>
2025-06-02 10:56:44 +0900

    fix: remove cross origin annotation

--- src/main/java/com/aivle/booksystem/controller/UserController.java
+++ src/main/java/com/aivle/booksystem/controller/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 import com.aivle.booksystem.domain.User;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
